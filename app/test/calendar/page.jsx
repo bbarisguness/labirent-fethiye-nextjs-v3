b96b8a0db3541030ed0b7f1a7fa287527a49c6b5
barış güneş <bbarisguness@msn.com>
2023-04-08 05:31:54 +0300

    test calendar yapıldı

--- app/test/calendar/page.jsx
+++ app/test/calendar/page.jsx
@@ -1,23 +1,23 @@
 'use client'
 import React from 'react'
 import Calendar from 'reactjs-availability-calendar'
-import "./page.module.css"
+import "./page.css"
 
 export default function calendar2() {
     const bookings = [
         {
-          from: new Date('2022-07-03'),
-          to: new Date('2022-07-30'),
+          from: new Date('2023-07-03'),
+          to: new Date('2023-07-28'),
           middayCheckout: true,
         },
         {
-          from: '2022-04-08',
-          to: '2022-04-13',
+          from: '2023-04-08',
+          to: '2023-04-13',
           middayCheckout: true,
         },
         {
-          from: '2022-09-03T19:20:35.593Z',
-          to: '2022-09-22T19:20:35.593Z',
+          from: '2023-09-03T19:20:35.593Z',
+          to: '2023-09-22T19:20:35.593Z',
           middayCheckout: false,
         },
       ]
